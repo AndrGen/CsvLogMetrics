b971c14aa5b762b6f5f39dcdd5a95fdfbd733a05
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-11-17 23:26:52 +0300

    добавлен скрипт для создания таблицы результатов

---
+++ src/main/resources/result.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS metric_result;
+
+CREATE TABLE metric_result (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  metric_name VARCHAR(50) NOT NULL,
+  metric_value INTEGER NOT NULL
+);
+
+INSERT INTO metric_result (metric_name, metric_value) VALUES
+  ('Event Count', 0),
+  ('Process Count', 0),
+  ('Avg Time Process', 0);
