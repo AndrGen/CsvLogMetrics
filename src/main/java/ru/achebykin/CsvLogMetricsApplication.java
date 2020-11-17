908447629f871d47448ab95107759c8a1b00115d
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-11-17 21:06:51 +0300

    init spring

---
+++ src/main/java/ru/achebykin/CsvLogMetricsApplication.java
@@ -0,0 +1,13 @@
+package ru.achebykin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvLogMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvLogMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/achebykin/CsvLogMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.achebykin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvLogMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
