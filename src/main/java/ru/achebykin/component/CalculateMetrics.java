19bc32ba333590055ec9d3a5a7ce811a6ee0f2d6
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-11-22 21:48:04 +0300

    Пул изменен на CompletableFuture

--- src/main/java/ru/achebykin/component/CalculateMetrics.java
+++ src/main/java/ru/achebykin/component/CalculateMetrics.java
@@ -12,8 +12,7 @@ import ru.achebykin.model.MetricValue;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class CalculateMetrics implements ICalculateMetrics {
@@ -25,59 +24,56 @@ public class CalculateMetrics implements ICalculateMetrics {
     /**
      * @param fileName
      */
-    public void calculate(String fileName)
-    {
-        ExecutorService pool = Executors.newSingleThreadExecutor();
-        pool.execute(() ->
-        {
-            MetricValue metricValue = new MetricValue();
-            SparkSession spark = SparkSession.builder()
-                    .master("local[1]")
-                    .appName("SparkByExamples.com")
-                    .getOrCreate();
-            SQLContext sqlContext = spark.sqlContext();
-            //read csv with options
-            sqlContext.read().option("delimiter", ",").option("header", true).csv(fileName).createOrReplaceTempView("hd");
+    public void calculate(String fileName) {
+        CompletableFuture.runAsync(
+                () ->
+                {
+                    MetricValue metricValue = new MetricValue();
+                    SparkSession spark = SparkSession.builder()
+                            .master("local[1]")
+                            .appName("SparkByExamples.com")
+                            .getOrCreate();
+                    SQLContext sqlContext = spark.sqlContext();
+                    //read csv with options
+                    sqlContext.read().option("delimiter", ",").option("header", true).csv(fileName).createOrReplaceTempView("hd");
 
-            Row row = sqlContext.sql(
-                    "select " +
-                            "   (select count(*) from hd) as EventCount," +
-                            "   (select " +
-                            "          count(GroupCaseId.CaseId) " +
-                            "                                         from (" +
-                            "                                            select CaseId" +
-                            "                                            from hd " +
-                            "                                            group by CaseId" +
-                            "                                          ) as GroupCaseId" +
-                            "   ) as ProcessCount, " +
-                            "    (select " +
-                            "          (avg(GroupCaseId.DateDiff)) / 1000 / 3600" +
-                            "                                         from (" +
-                            "                                            select " +
-                            "                                               max(unix_timestamp(CompleteTimestamp)) - " +
-                            "                                               min(unix_timestamp(CompleteTimestamp)) as DateDiff" +
-                            "                                            from hd " +
-                            "                                            group by CaseId" +
-                            "                                          ) as GroupCaseId" +
-                            "   ) as AvgTimeProcess "
-            ).first();
-            metricValue.setEventCount(Long.toString(row.getAs("EventCount")));
-            metricValue.setProcessCount(Long.toString(row.getAs("ProcessCount")));
-            metricValue.setAvgTimeProcess(Double.toString(row.getAs("AvgTimeProcess")));
+                    Row row = sqlContext.sql(
+                            "select " +
+                                    "   (select count(*) from hd) as EventCount," +
+                                    "   (select " +
+                                    "          count(GroupCaseId.CaseId) " +
+                                    "                                         from (" +
+                                    "                                            select CaseId" +
+                                    "                                            from hd " +
+                                    "                                            group by CaseId" +
+                                    "                                          ) as GroupCaseId" +
+                                    "   ) as ProcessCount, " +
+                                    "    (select " +
+                                    "          (avg(GroupCaseId.DateDiff)) / 1000 / 3600" +
+                                    "                                         from (" +
+                                    "                                            select " +
+                                    "                                               max(unix_timestamp(CompleteTimestamp)) - " +
+                                    "                                               min(unix_timestamp(CompleteTimestamp)) as DateDiff" +
+                                    "                                            from hd " +
+                                    "                                            group by CaseId" +
+                                    "                                          ) as GroupCaseId" +
+                                    "   ) as AvgTimeProcess "
+                    ).first();
+                    metricValue.setEventCount(Long.toString(row.getAs("EventCount")));
+                    metricValue.setProcessCount(Long.toString(row.getAs("ProcessCount")));
+                    metricValue.setAvgTimeProcess(Double.toString(row.getAs("AvgTimeProcess")));
 
-            saveResult(metricValue);
-        });
-        pool.shutdown();
+                    saveResult(metricValue);
+                });
     }
 
-    private void saveResult(@org.jetbrains.annotations.NotNull MetricValue metricValue)
-    {
-            Map<String, String> metricMap = new HashMap<>();
-            metricMap.put("Event Count", metricValue.getEventCount());
-            metricMap.put("Process Count", metricValue.getProcessCount());
-            metricMap.put("Avg Time Process", metricValue.getAvgTimeProcess());
-            metricComponent.addMetricResult(metricMap);
+    private void saveResult(@org.jetbrains.annotations.NotNull MetricValue metricValue) {
+        Map<String, String> metricMap = new HashMap<>();
+        metricMap.put("Event Count", metricValue.getEventCount());
+        metricMap.put("Process Count", metricValue.getProcessCount());
+        metricMap.put("Avg Time Process", metricValue.getAvgTimeProcess());
+        metricComponent.addMetricResult(metricMap);
 
-            logger.debug("Result have been saved");
+        logger.debug("Result have been saved");
     }
 }
